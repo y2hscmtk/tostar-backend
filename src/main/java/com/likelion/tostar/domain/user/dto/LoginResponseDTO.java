0be247b50c51a86a2137612a961e09d3766ca2d8
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-10-18 13:30:59 +0900

    🎨 [add] LoginResponseDTO 작성

---
+++ src/main/java/com/likelion/tostar/domain/user/dto/LoginResponseDTO.java
@@ -0,0 +1,15 @@
+package com.likelion.tostar.domain.user.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * 로그인 성공시 일부 개인 정보 반환
+ */
+@Data
+@Builder
+public class LoginResponseDTO {
+    private String petName; // 애완동물 이름
+    private String userName; // 사용자 이름
+    private String email; // 사용자 이메일
+}
