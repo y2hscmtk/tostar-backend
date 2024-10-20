a51934ffb46dcdd231e2b76f0e946b8989ddb633
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-10-20 22:16:05 +0900

    ✨ [feat] 커뮤니티 미리보기 랜덤 반환 메소드 작성
    CommunityRepository - getRandomPreviews() : 커뮤니티 미리보기 랜덤 3개 반환

--- src/main/java/com/likelion/tostar/domain/community/repository/CommunityRepository.java
+++ src/main/java/com/likelion/tostar/domain/community/repository/CommunityRepository.java
@@ -2,7 +2,14 @@ package com.likelion.tostar.domain.community.repository;
 
 
 import com.likelion.tostar.domain.community.entity.Community;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommunityRepository extends JpaRepository<Long, Community> {
+    /**
+     * 커뮤니티 미리보기 랜덤 3개 반환
+     */
+    @Query(value = "SELECT * FROM Community ORDER BY RAND() LIMIT 3", nativeQuery = true)
+    List<Community> getRandomPreviews();
 }
