cec4728b7e1e0867d4e41a983e945d38e352fa99
khyaejin <olivia019182@naver.com>
2024-11-03 19:56:18 +0900

    🚨 [Fix] 편지 전송 api 수정

--- src/main/java/com/likelion/tostar/domain/letter/service/LetterService.java
+++ src/main/java/com/likelion/tostar/domain/letter/service/LetterService.java
@@ -1,11 +1,11 @@
 package com.likelion.tostar.domain.letter.service;
 
-import com.likelion.tostar.domain.letter.dto.LetterPostDto;
+import com.likelion.tostar.domain.letter.dto.LetterPostRequestDto;
 import org.springframework.http.ResponseEntity;
 
 public interface LetterService {
     // 편지 생성
-    ResponseEntity<?> post(Long userId, LetterPostDto letterPostDto);
+    ResponseEntity<?> post(Long userId, LetterPostRequestDto letterPostRequestDto);
 
     // 편지 목록 전체 조회
     ResponseEntity<?> searchList(Long userId, int page, int size);
